bc0efba5423e0bcef36480807e12394aec489898
Deepak Malik <deepak164malik@gmail.com>
2021-11-30 17:47:52 +0530

    Create configs.py
    This file help in getting basic endpoint for the API

---
+++ tests/configs.py
@@ -0,0 +1,16 @@
+import requests
+import pytest
+import configparser
+
+
+def get_url():
+    config = configparser.ConfigParser()
+    config.read('pytest.ini')
+    baseurl = config.get('API', 'assets_endpoint')
+
+    return baseurl
+
+
+
+
+
