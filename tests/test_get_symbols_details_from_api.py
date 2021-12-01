be4981636fb671fb76b154fb1ee6c445fa78d79c
Deepak Malik <deepak164malik@gmail.com>
2021-12-01 19:02:21 +0530

    Update test_get_symbols_details_from_api.py

--- tests/test_get_symbols_details_from_api.py
+++ tests/test_get_symbols_details_from_api.py
@@ -4,6 +4,8 @@ from .configs import *
 import logging
 import pandas as pd
 import pytest
+import github
+
 
 LOGGER = logging.getLogger('pytest.ini')
 
@@ -31,7 +33,7 @@ def test_get_symbols_from_api(url):
 @pytest.mark.smoke
 def test_symbol_details_from_api(url):
     error_symbol_list = []
-    for item in test_get_symbols_from_api(url):
+    for item in test_get_symbols_from_api(url)[0:5]:
 
         symbol_api_url = f"{url}/{item}"
         symbol_api_url_response = requests.get(symbol_api_url)
@@ -56,12 +58,12 @@ def test_symbol_details_from_api(url):
 
                     assert_that(float(price_in_rowan)).is_greater_than(0)
     df = pd.DataFrame(error_symbol_list, columns=['Symbols'])
-    # file_path = "output_data"
+    file = "symbols_error_to_csv_file.csv"
+    csv_file = df.to_csv(file, index=False)
 
-    df.to_csv(index=False)
-    compression_opts = dict(method='zip',
-                            archive_name='out.csv')
-    df.to_csv('out.zip', index=False,
-              compression=compression_opts)
+    from github.MainClass import Github
+    g = Github('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')
+    g.load(csv_file)
 
-    # LOGGER.info(f"file is created: {file}")
+
+  
